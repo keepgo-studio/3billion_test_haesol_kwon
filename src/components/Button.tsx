70e53e570fec84634e106ef8223690103771cf51
keepgo-studio <keepgo.studio@gmail.com>
2023-11-01 00:12:46 +0900

    [Add] tailwind plugin, clsx

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,10 +1,11 @@
+import clsx from "clsx";
 import React from "react";
 
 const Button = ({
   children,
-  theme = 'primary',
+  theme = "primary",
   onClick,
-  isDisabled
+  isDisabled,
 }: {
   children: React.ReactNode;
   theme?: "primary" | "gray";
@@ -12,9 +13,9 @@ const Button = ({
   isDisabled?: boolean;
 }) => {
   const getThemeStyle = () => {
-    const sharedStyles = `w-72 h-12 font-light text-base flex-center rounded-full hover:text-white hover:bg-pblue duration-300
-      ${isDisabled ? 'text-white !bg-gray-300 cursor-not-allowed' : ''}
-    `;
+    const sharedStyles = clsx("w-72 h-12 font-light text-base flex-center rounded-full hover:text-white hover:bg-pblue duration-300", [
+      isDisabled ? "text-white !bg-gray-300 cursor-not-allowed" : ""
+    ]);
 
     switch (theme) {
       case "gray":
@@ -30,17 +31,14 @@ const Button = ({
           text-white
           ${sharedStyles}
         `;
-      }
     }
-    
-    return (
-      <button
-        onClick={onClick}
-        className={getThemeStyle()}
-      >
-        {children}
-      </button>
-    );
+  };
+
+  return (
+    <button onClick={onClick} className={getThemeStyle()}>
+      {children}
+    </button>
+  );
 };
 
 export default Button;
